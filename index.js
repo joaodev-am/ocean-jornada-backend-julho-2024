c39b77f4f2aaf56069643618c62ba960cd697d2f
joaodev-am <joaoabreu2115@gmail.com>
2024-07-03 12:32:07 +0000

    Alterações 1.1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.send('Hello World')
 })
+app.get("/oi", function (req, res) {
+  res.send('Olá, mundo!')
+}
 
 app.listen(3000)
